7b3c9c7f7417dca16c0f8290f1db8028be74e21b
Thiago Oliveira <thiagomorette@gmail.com>
2021-04-03 11:58:59 -0300

    removed coverage folder for tests info; added test for AuthenticateUserService

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,36 +1,24 @@
 import MockUserRepository from '@modules/users/repositories/mocks/MockUserRepository';
-import AppError from '@shared/errors/AppError';
+import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
-   it('should be able to create a new user', async () => {
+   it('should be able to authenticate', async () => {
       const mockRepository = new MockUserRepository();
-      const createAppointmentService = new CreateUserService(mockRepository);
-      const user = await createAppointmentService.execute({
+      const createUser = new CreateUserService(mockRepository);
+      const authenticateUserService = new AuthenticateUserService(mockRepository);
+
+      await createUser.execute({
          name: 'thiago',
          email: 'thiago@email.com',
          password: '123456',
       });
 
-      expect(user).toHaveProperty('id');
-      expect(user.name).toBe('thiago');
-   });
-
-   it('should not be able to create a new user with an existing email', async () => {
-      const mockRepository = new MockUserRepository();
-      const createAppointmentService = new CreateUserService(mockRepository);
-      await createAppointmentService.execute({
-         name: 'thiago',
+      const user = await authenticateUserService.execute({
          email: 'thiago@email.com',
          password: '123456',
       });
 
-      expect(
-         createAppointmentService.execute({
-            name: 'thiago',
-            email: 'thiago@email.com',
-            password: '123456',
-         }),
-      ).rejects.toBeInstanceOf(AppError);
+      expect(user).toHaveProperty('token');
    });
 });
