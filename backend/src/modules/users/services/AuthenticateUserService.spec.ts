31f8530b87f6b46971482302e7d23508b2502aac
Thiago Oliveira <thiagomorette@gmail.com>
2021-04-05 21:38:14 -0300

    created two more tests to achieve 100% test coverage in AuthenticateUserService

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import MockUserRepository from '@modules/users/repositories/mocks/MockUserRepository';
 import MockHashProvider from '@modules/users/providers/mocks/MockHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -10,18 +11,51 @@ describe('AuthenticateUser', () => {
       const createUser = new CreateUserService(mockRepository, mockHashProvider);
       const authenticateUserService = new AuthenticateUserService(mockRepository, mockHashProvider);
 
+      const user = await createUser.execute({
+         name: 'thiago',
+         email: 'thiago@email.com',
+         password: '123456',
+      });
+
+      const response = await authenticateUserService.execute({
+         email: 'thiago@email.com',
+         password: '123456',
+      });
+
+      expect(response).toHaveProperty('token');
+      expect(response.user).toEqual(user);
+   });
+
+   it('should not be able to authenticate with non existing user', async () => {
+      const mockRepository = new MockUserRepository();
+      const mockHashProvider = new MockHashProvider();
+      const authenticateUserService = new AuthenticateUserService(mockRepository, mockHashProvider);
+
+      expect(
+         authenticateUserService.execute({
+            email: 'thiago@email.com',
+            password: '123456',
+         }),
+      ).rejects.toBeInstanceOf(AppError);
+   });
+
+   it('should not be able to authenticate whit wrong password', async () => {
+      const mockRepository = new MockUserRepository();
+      const mockHashProvider = new MockHashProvider();
+      const createUser = new CreateUserService(mockRepository, mockHashProvider);
+      const authenticateUserService = new AuthenticateUserService(mockRepository, mockHashProvider);
+
       await createUser.execute({
          name: 'thiago',
          email: 'thiago@email.com',
          password: '123456',
       });
 
-      const user = await authenticateUserService.execute({
-         email: 'thiago@email.com',
-         password: '123456',
-      });
-
-      expect(user).toHaveProperty('token');
-      expect(user.user).toEqual(user);
+      expect(
+         authenticateUserService.execute({
+            email: 'thiago@email.com',
+            password: 'wrong-password',
+         }),
+      ).rejects.toBeInstanceOf(AppError);
    });
 });
